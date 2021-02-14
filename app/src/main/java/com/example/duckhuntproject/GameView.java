3b7d0cae9e348a182c4d4607cdb6abf36fe396f3
grigorijbusahin <gbusaxin@gmail.com>
2021-02-14 13:12:35 +0200

    Added full screen and changed way of getting size screen

--- app/src/main/java/com/example/duckhuntproject/GameView.java
+++ app/src/main/java/com/example/duckhuntproject/GameView.java
@@ -1,23 +1,35 @@
 package com.example.duckhuntproject;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.view.Display;
 import android.view.View;
 
 public class GameView extends View {
 
     Bitmap background;
+    Rect rect;
+    int dWidth, dHeight;
 
     public GameView(Context context) {
         super(context);
         background = BitmapFactory.decodeResource(getResources(),R.drawable.background_day);
+        Display display = ((Activity)getContext()).getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        dWidth = size.x;
+        dHeight = size.y;
+        rect = new Rect(0,0,dWidth,dHeight);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawBitmap(background,0,0,null);
+        canvas.drawBitmap(background,null,rect,null);
     }
 }
